f99fc3a806b81e5e9732f6e566efba66deb627c5
pmehu <patelmehul8087@gmail.com>
2024-07-17 10:17:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,53 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
-const basicAuth = require('basic-auth');
+const mysql = require('mysql2/promise');
 
 const app = express();
 const PORT = process.env.PORT || 8080; // Default to 8080 for Cloud Run
 const HUGGING_FACE_API_KEY = process.env.HUGGING_FACE_API_KEY;
-const USERNAME = process.env.BASIC_AUTH_USERNAME || 'admin';
-const PASSWORD = process.env.BASIC_AUTH_PASSWORD || 'admin';
 
-const auth = (req, res, next) => {
-  const user = basicAuth(req);
-  if (!user || user.name !== USERNAME || user.pass !== PASSWORD) {
+let USERNAME = '';
+let PASSWORD = '';
+
+// Function to get credentials from MySQL
+async function getCredentials() {
+  const connection = await mysql.createConnection({
+    host: '34.16.11.198',
+    user: 'root',
+    password: 'Sathi@1993',
+    database: 'demo-gen',
+  });
+
+  const [rows, fields] = await connection.execute('SELECT username, password FROM users WHERE id = 1');
+  if (rows.length > 0) {
+    USERNAME = rows[0].username;
+    PASSWORD = rows[0].password;
+  }
+
+  await connection.end();
+}
+
+// Middleware for basic authentication
+const auth = async (req, res, next) => {
+  await getCredentials();
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) {
     res.set('WWW-Authenticate', 'Basic realm="401"');
     res.status(401).send('Authentication required.');
     return;
   }
+
+  const base64Credentials = authHeader.split(' ')[1];
+  const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+  const [username, password] = credentials.split(':');
+
+  if (username !== USERNAME || password !== PASSWORD) {
+    res.set('WWW-Authenticate', 'Basic realm="401"');
+    res.status(401).send('Authentication required.');
+    return;
+  }
+
   next();
 };
 

be6cc035d1191e1a5ad2f8bfc56215c942fc8a56
pmehu <patelmehul8087@gmail.com>
2024-07-18 16:46:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
+const basicAuth = require('basic-auth'); // Add this line to import basic-auth
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
 const app = express();
